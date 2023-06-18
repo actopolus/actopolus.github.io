dc329f18d377833904a9b33ac8740f2b4ffbee7f
Bereznev Vitaly <30438018+actopolus@users.noreply.github.com>
2023-06-18 14:03:53 +0600

    Update bundle.js

--- bundle.js
+++ bundle.js
@@ -1,1 +1,36 @@
+function getQueryParams(url) {
+      const paramArr = url.slice(url.indexOf('?') + 1).split('&');
+      const params = {};
+      paramArr.map(param => {
+        const [key, val] = param.split('=');
+        params[key] = decodeURIComponent(val);
+      })
+      return params;
+    }
 
+window.onload = function() {
+       const projects = {
+        "jr"          : "Jetpack Racing",
+        "kd"          : "Knock em down 3D",
+        "ct"          : "Cheating Test",
+        "jumpinghoop" : "Jumping Hoop",
+        "ts"          : "Tricky Stack",
+        "pr"          : "Pin Race",
+        "egd"         : "Epic Giants Duel 3D",
+        "hbr"         : "Hot Babysitter Run",
+        "hoof"        : "Hoof Restoration 3D",
+        "train"       : "Train Race",
+        "shop"        : "Shopping Madness: Merge Master",
+        "hack"        : "Hack and Rescue",
+        "room"        : "Home Room Runner",
+        "vice"        : "Vice City Crime: Theft Auto",
+      };
+      
+      const params = getQueryParams(window.location.search);
+      const title = projects[params["pr"] ?? "kd"];
+      const titles = document.getElementsByClassName("project-name");
+      
+      for (let i = 0; i < titles.length; i++) {
+        titles[i].innerText = title;
+      }
+    };
